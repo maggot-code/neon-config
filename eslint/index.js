5e5a250760111d0f01719250ba800e459391597e
maggot_code <63178315+maggot-code@users.noreply.github.com>
2022-06-12 05:11:57 +0800

    fix : publish npm package

--- eslint/index.js
+++ eslint/index.js
@@ -3,8 +3,9 @@
  * @Author: maggot-code
  * @Date: 2022-06-12 03:52:45
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-06-12 03:53:07
+ * @LastEditTime: 2022-06-12 03:55:22
  * @Description: eslint config package
  */
-const Name = "@neon-eslint-config";
-console.log(Name);
+module.exports = {
+    
+};
---
+++ vscode/index.js
@@ -0,0 +1,11 @@
+/*
+ * @FilePath: /neon-config/vscode/index.js
+ * @Author: maggot-code
+ * @Date: 2022-06-12 05:08:48
+ * @LastEditors: maggot-code
+ * @LastEditTime: 2022-06-12 05:08:54
+ * @Description: 
+ */
+module.exports = {
+    
+};
