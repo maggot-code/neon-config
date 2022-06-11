859eea29833cbd38385de90f6a0bb6b4e611536f
maggot_code <63178315+maggot-code@users.noreply.github.com>
2022-06-12 03:54:01 +0800

    feat :+1:

---
+++ eslint/index.js
@@ -0,0 +1,10 @@
+/*
+ * @FilePath: /neon-config/eslint/index.js
+ * @Author: maggot-code
+ * @Date: 2022-06-12 03:52:45
+ * @LastEditors: maggot-code
+ * @LastEditTime: 2022-06-12 03:53:07
+ * @Description: eslint config package
+ */
+const Name = "@neon-eslint-config";
+console.log(Name);
