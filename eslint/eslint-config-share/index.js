61d4659eab5a639585129ecea105d48469e8d2cd
maggot_code <63178315+maggot-code@users.noreply.github.com>
2022-06-19 23:07:28 +0800

    fix :-1:

--- eslint/eslint-config-share/index.js
+++ eslint/eslint-config-share/index.js
@@ -3,7 +3,44 @@
  * @Author: maggot-code
  * @Date: 2022-06-18 17:34:23
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-06-18 17:34:36
+ * @LastEditTime: 2022-06-19 20:25:58
  * @Description: 
  */
-console.log('eslint config share');
+import { default as PossibleErrors } from "./possible-errors";
+import { default as BestPractices } from "./best-practices";
+import { default as Variable } from "./variable";
+import { default as Browser } from "./browser";
+import {default as CodeStyle} from "./code-style";
+import { default as Es6 } from "./es6";
+
+module.exports = {
+    parserOptions: {
+        ecmaVersion: 2022,
+        ecmaFeatures: {
+            jsx: true,
+        },
+        sourceType: 'module',
+    },
+    env: {
+        es2021: true,
+        node: true
+    },
+    globals: {
+        window: 'readonly'
+    },
+    plugins: [],
+    rules: {
+        // 可能会存在的错误
+        ...PossibleErrors,
+        // 最佳实践
+        ...BestPractices,
+        // 变量相关
+        ...Variable,
+        // NodeJS 或者 CommonJS相关
+        ...Browser,
+        // 代码风格相关
+        ...CodeStyle,
+        // ES6相关
+        ...Es6
+    }
+}
--- eslint/index.js
+++
@@ -1,9 +0,0 @@
-/*
- * @FilePath: /neon-config/eslint/index.js
- * @Author: maggot-code
- * @Date: 2022-06-18 18:02:13
- * @LastEditors: maggot-code
- * @LastEditTime: 2022-06-18 18:02:22
- * @Description: 
- */
-console.log('eslint config');
