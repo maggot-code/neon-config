90745b678e31699a29447c544b99e076ac2e880e
maggot_code <wowhuanhuan233@163.com>
2022-06-23 17:22:52 +0800

    feat :+1:

---
+++ eslint/eslint-config-prettier/index.js
@@ -0,0 +1,9 @@
+/*
+ * @FilePath: \neon-config\eslint\eslint-config-prettier\index.js
+ * @Author: maggot-code
+ * @Date: 2022-06-23 17:21:58
+ * @LastEditors: maggot-code
+ * @LastEditTime: 2022-06-23 17:22:06
+ * @Description: 禁用掉 ESLint 与 Prettier 中冲突的部分
+ */
+module.exports = {}
