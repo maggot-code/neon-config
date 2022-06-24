65f0f283fcc96850ce3f8c7e5dab40c4c74bedce
maggot_code <wowhuanhuan233@163.com>
2022-06-24 18:01:49 +0800

    fix :-1:

--- eslint/test/index.js
+++ eslint/test/index.js
@@ -2,8 +2,8 @@
  * @FilePath: \neon-config\eslint\test\index.js
  * @Author: maggot-code
  * @Date: 2022-06-23 09:13:25
- * @LastEditors: maggot-code
- * @LastEditTime: 2022-06-24 17:29:06
- * @Description: 
+ * @LastEditors: zhangyang
+ * @LastEditTime: 2022-06-24 17:57:43
+ * @Description:
  */
-const a ='sd';
+const a = "sd";
