faa08a42bf591436ea30ffb9eb00433d4c240b18
maggot_code <63178315+maggot-code@users.noreply.github.com>
2022-06-28 00:16:28 +0800

    fix :-1:

--- eslint/test/index.vue
+++ eslint/test/index.vue
@@ -3,7 +3,7 @@
  * @Author: maggot-code
  * @Date: 2022-06-27 20:38:54
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-06-28 00:09:57
+ * @LastEditTime: 2022-06-28 00:12:13
  * @Description: 
 -->
 <template>
@@ -11,38 +11,39 @@
 </template>
 
 <script>
-    export default {
-        name: '',
-        components: {},
-        mixins: [],
-        props: {},
-        data() {
-            //这里存放数据
-            return {
-                value: true
-            };
-        },
-        //监听属性 类似于data概念
-        computed: {},
-        //监控data中的数据变化
-        watch: {
-            value: (newVal) => {
-                console.log(newVal);
-            }
-        },
-        //生命周期 - 创建完成（可以访问当前this实例）
-        created() { },
-        //生命周期 - 挂载完成（可以访问DOM元素）
-        mounted() { },
-        beforeCreate() { }, //生命周期 - 创建之前
-        beforeMount() { }, //生命周期 - 挂载之前
-        beforeUpdate() { }, //生命周期 - 更新之前
-        updated() { }, //生命周期 - 更新之后
-        beforeUnmount() { }, //生命周期 - 销毁之前
-        unmounted() { }, //生命周期 - 销毁完成
-        activated() { },
-        //方法集合
-        methods: {}, //如果页面有keep-alive缓存功能，这个函数会触发
-    };
+export default {
+    name: '',
+    components: {},
+    mixins: [],
+    props: {},
+    data() {
+        //这里存放数据
+        return {
+            value: true
+        };
+    },
+    //监听属性 类似于data概念
+    computed: {},
+    //监控data中的数据变化
+    watch: {
+        value: (newVal) => {
+            console.log(newVal);
+        }
+    },
+    //生命周期 - 创建完成（可以访问当前this实例）
+    created() { },
+    //生命周期 - 挂载完成（可以访问DOM元素）
+    mounted() { },
+    beforeCreate() { }, //生命周期 - 创建之前
+    beforeMount() { }, //生命周期 - 挂载之前
+    beforeUpdate() { }, //生命周期 - 更新之前
+    updated() { }, //生命周期 - 更新之后
+    beforeUnmount() { }, //生命周期 - 销毁之前
+    unmounted() { }, //生命周期 - 销毁完成
+    activated() { },
+    //方法集合
+    methods: {}, //如果页面有keep-alive缓存功能，这个函数会触发
+};
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+</style>
