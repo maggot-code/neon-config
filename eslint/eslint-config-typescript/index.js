d3af41f70412a30e02277d0115f9f29d3d1252ba
maggot_code <wowhuanhuan233@163.com>
2022-06-27 18:46:29 +0800

    Update index.js

--- eslint/eslint-config-typescript/index.js
+++
@@ -1,9 +0,0 @@
-/*
- * @FilePath: /neon-config/eslint/eslint-config-typescript/index.js
- * @Author: maggot-code
- * @Date: 2022-06-18 17:35:19
- * @LastEditors: maggot-code
- * @LastEditTime: 2022-06-18 17:35:25
- * @Description: 
- */
-console.log('eslint config typescript');
